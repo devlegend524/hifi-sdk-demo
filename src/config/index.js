9add1a10ad9348251e8188f93ff084d103ec6d18
tonychan524 <Anthony@HiFiGamingSociety.org>
2022-11-23 14:44:58 +0900

    updated sdk api config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-// export const API_URL = "https://18.134.157.106:443";
+export const API_URL = "https://18.134.157.106:443";
 // export const API_URL = "https://146.19.207.168:44344";
-export const API_URL = "https://localhost:44344";
+// export const API_URL = "https://localhost:44344";
 export const MINIMUM_PLAY_FOR_REWARD = 10
